bc22fdfecebd4852d99b4ae2f8081f19e0248cf8
Weverton Lima <wevertonslima@gmail.com>
2024-05-01 18:28:41 -0300

    updated env example configurations

--- config/ollama-laravel.php
+++ config/ollama-laravel.php
@@ -5,7 +5,7 @@
 return [
     'model' => env('OLLAMA_MODEL', 'llama3'),
     'url' => env('OLLAMA_URL', 'http://127.0.0.1:11434'),
-    'default_prompt' => env('OLLAMA_DEFAULT_PROMPT', 'Ola, sou um bot llama3 rodando no pc do Weverton, posso ajudar?'),
+    'default_prompt' => env('OLLAMA_DEFAULT_PROMPT', 'Ola, posso ajudar?'),
     'connection' => [
         'timeout' => env('OLLAMA_CONNECTION_TIMEOUT', 300),
     ],
